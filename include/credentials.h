d946d71dbc21feac1f24a08e6450666a7bceab3b
Dennis Adrian Schneider <dennis.schneider@xms-dev.com>
2022-06-26 15:40:47 +0200

    Update credentials.h

--- include/credentials.h
+++ include/credentials.h
@@ -5,9 +5,9 @@
 
 #include <ESP8266WiFi.h>
 
-char ssid_wifi[] = "your_ssid";
-char passwd[] = "your_password";
+String ssid_wifi = "your_ssid";
+String passwd = "your_password";
 //Database Server Constants
-const IPAddress dbServerIP(0, 0, 0, 0); //Replace with your destination IP.
-const uint16_t dbPort = 0; //Replace with your destination port.
+String authServerIP = "your_auth_server_ip"; //Replace with your destination IP.
+String authServerPort = "your_port"; //Replace with your destination port.
 #endif //SV_CREDENTIALS_H
